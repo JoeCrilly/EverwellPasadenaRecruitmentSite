894f436824bdd4e4a717de8b11e2a49983da08de
JoeCrilly <joe.crilly503@gmail.com>
2015-08-31 15:27:54 -0700

    Initial Upload

---
+++ js/LogoSlider.js
@@ -0,0 +1,76 @@
+    /*=====Logo=====*/
+    $('.holdingboxA').hover(function(){
+        $('.rightboxA').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightboxA').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 0=====*/
+	$('.holdingbox0').hover(function(){
+        $('.rightbox0').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox0').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 1=====*/
+	$('.holdingbox1').hover(function(){
+        $('.rightbox1').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox1').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 2=====*/
+	$('.holdingbox2').hover(function(){
+        $('.rightbox2').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox2').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 3=====*/
+	$('.holdingbox3').hover(function(){
+        $('.rightbox3').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox3').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 4=====*/
+	$('.holdingbox4').hover(function(){
+        $('.rightbox4').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox4').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 5=====*/
+	$('.holdingbox5').hover(function(){
+        $('.rightbox5').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox5').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 6=====*/
+	$('.holdingbox6').hover(function(){
+        $('.rightbox6').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox6').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 7=====*/
+	$('.holdingbox7').hover(function(){
+        $('.rightbox7').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox7').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 8=====*/
+	$('.holdingbox8').hover(function(){
+        $('.rightbox8').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox8').animate({width: '0'}, 500)
+    });
+	
+	/*=====Logo 9=====*/
+	$('.holdingbox9').hover(function(){
+        $('.rightbox9').animate({width: '1260px', height: '400px'}, 500)
+    }, function(){
+        $('.rightbox9').animate({width: '0'}, 500)
+    });
